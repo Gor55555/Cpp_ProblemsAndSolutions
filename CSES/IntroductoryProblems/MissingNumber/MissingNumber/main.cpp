520db4081939ec70c656fcd4ab2eaebeb409eba3
Gor55555 <gor.khachatryan.00000@gmail.com>
2024-10-24 15:53:02 +0400

    MissingNumber

---
+++ CSES/IntroductoryProblems/MissingNumber/MissingNumber/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+void My_Missing_Number(int n)
+{
+    int s = 0;
+    std::cout << std::setw(15) << std::left << "Input Element: ";
+    for (int i = 1; i < n; ++i)
+    {
+        int k;
+        std::cin >> k;
+        s += k;
+    }
+    std::cout << std::setw(15) << std::left << "Output: ";
+    std::cout << n * (n + 1) / 2 - s;
+}
+
+int main()
+{
+    int n;
+    std::cout << std::setw(15) << std::left << "Input line: ";
+    std::cin >> n;
+    My_Missing_Number(n);
+}
